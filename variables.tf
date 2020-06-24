d2615cb049755ced49df960657f422677768b684
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-06-24 13:21:23 -0700

    update variables for oidc/dns

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region"{
+  type        = string
+  description = "AWS region to use"
+  default     = "us-west-2"
+}
+
 variable "availability_zone" {
   type        = string
   description = "AWS Availability Zone to use"
@@ -6,7 +12,7 @@ variable "availability_zone" {
 
 variable "instance_type" {
   type        = string
-  description = "The AWS EC2 tier to use for the DB instances."
+  description = "The AWS EC2 tier to use for the instance."
   default     = "t2.small"
 }
 
@@ -25,5 +31,26 @@ variable "aws_user"{
 variable "private_key_path"{
   type        = string
   description = "full path to the filename for the private key to be used by ansible"
-  default     ="/Users/jeff/.ssh/jab.pem"
+  default     = "/Users/jeff/.ssh/jab.pem"
+}
+
+variable "dns_name" {
+  type        = string
+  description = "the dns name for this deployment (sent fo flask as SERVER_NAME)"
+}
+
+variable "tls_certificate_arn" {
+  type        = string
+  description = "The arn of the tls/ssl certificate to attach to the load balancer"
+}
+
+variable "oidc_client_id" {
+  type        = string
+  description = "The client ID given to you by your identity provider (IDP)"
+}
+
+variable "preferred_url_scheme" {
+  type        = string
+  description = "http for non ssl, https for ssl"
+  default     = "http"
 }
